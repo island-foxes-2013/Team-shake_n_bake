2a9e9470213c1bbcdde026b6a733af2b9b058bc3
Apprentice <apprentice@devbootcamp.com>
2013-08-07 10:39:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShakeNBake::Application.config.secret_token = 'd1012e206021d5192653938d724ebb6fdca28be40e44961083a81eadfae261e04f4a384f43d3d1243be9a153a76bfb726b1ad9011dccfabb0ef858bfc772ffea'
