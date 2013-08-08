872dd049174dcc15396ff16bf03586c411acd4d3
Christine <cmarquardt86@gmail.com>
2013-08-07 18:04:51 -0700

    added a create answer form and route

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,18 @@
 class AnswersController < ApplicationController
   def index
   end
+
+  def new
+  	@answer = Answer.new
+  end
+
+  def create
+  	answer = Answer.new(params[:answer])
+  	answer.save
+  	redirect_to question_path(@question)
+  end
+
+  def destroy
+  end
+
 end
