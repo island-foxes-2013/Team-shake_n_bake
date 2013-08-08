f9636f1f8a1902f1ab9c9556d70ae90622992ef9
Apprentice <apprentice@devbootcamp.com>
2013-08-08 10:11:35 -0700

    dont know

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
   validates :username, :email, :password, :password_confirmation, presence: true
+  validates :username, :email, uniqueness: true
   has_secure_password
   attr_accessible :username, :email, :password, :password_confirmation
+
 
   
 end
