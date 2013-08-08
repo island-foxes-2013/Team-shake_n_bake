e16fa1e5dca8f1503cc6019d8862cff271e7510f
Gregory Varias <gregvarias@gmail.com>
2013-08-08 08:46:10 -0700

    Added test to question_spec.rb

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,40 +3,21 @@ require 'spec_helper'
 describe Question do
 	let(:question) {Question.new(title:"demo title", body:"this is a demo body")}
 
-	# context "when title is empty" do
-	# 	it "should not save" do
-	# 		Question.create(body:"text").should be_nil
-	# 	end
-	# end
+	context "when title is empty" do
+		it "should not save" do
+			Question.create(body:"text").id.should be_nil
+		end
+	end
+
+	context "when body is empty" do
+		it "should not save" do
+			Question.create(title:"text").id.should be_nil
+		end
+	end
+
+	context "both fields are filled in" do
+		it "should save" do
+			expect {question.save}.to be_true
+		end
+	end
 end
-
-
-
-
-
-# # FOR REFERENCE
-# describe User do
-#   let (:user) { User.new }
-
-#   context "when name is empty" do
-#     it "should not be valid" do
-#       expect(user.valid?).to be_false
-#     end
-
-#     it "should not save" do
-#       expect(user.save).to be_false
-#     end
-#   end
-
-#   context "when name is not empty" do
-#     let (:user) { User.new(:name => "Alex") }
-
-#     it "should be valid" do
-#       expect(user.valid?).to be_true
-#     end
-
-#     it "should save" do
-#       expect(user.save).to be_true
-#     end
-#   end
-# end
