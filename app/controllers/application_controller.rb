c138c1c5d5a0f926ffac7c1ae97d35f8b0b7c1fe
Gregory Varias <gregvarias@gmail.com>
2013-08-07 21:39:02 -0700

    Fixed layout to not use helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+		User.find_by_id(session[:id])
+	end
+
+	def logged_in?
+		session[:id]
+	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,3 @@
 module UsersHelper
 
-	def current_user
-		User.find_by_id(session[:id])
-	end
-
-	def logged_in?
-		!session[:id].nil?
-	end
 end
