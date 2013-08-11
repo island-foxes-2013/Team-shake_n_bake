4d5553cb7d524fbfb03f4d5806eb786efe1d49d5
Apprentice <apprentice@devbootcamp.com>
2013-08-11 15:32:05 -0700

    question show page alsmot done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,40 +1,14 @@
 class CommentsController < ApplicationController
   def create
-  	 	p params[:question_id]
   	 	user = User.find(session[:id])
-  	 if params.has_key?('question_id')
-       @comment = Question.find(params[:question_id]).comments.create(content: params[:comment], user_id: user.id)
-       # render json: render_to_string(partial: 'form', locals: { bananas: 'hello' }).to_json
-       # format.json { render json: @comment, status: :create, location: @comment}   
+  	if params.has_key?('question_id')
+      @comment = Question.find(params[:question_id]).comments.create(content: params[:comment], user_id: user.id)  
       render json: { comment: @comment, user: user }
-     end
-# redirect_to question_path(params[:question_id])
- 
-    # answer = Answer.find_by_id(params[:answer_id])
-    # answer.comments.create(user_id: session[:id], content: params[:comment][:content])
-    # redirect_to question_path(answer.question_id)
+    elsif params.has_key?('answer_id')
+      @comment = Answer.find(params[:answer_id]).comments.create(content: params[:comment], user_id: user.id)  
+      render json: { comment: @comment, user: user }
+    else
+      render json: { error: 'something went terribly wrong'}
+    end
   end
 end
-
-# def create
-
-#     if params.has_key?('question_id')
-#       Question.find(params[:question_id]).comments.create(content: params[:comment][:content], user_id: session[:id])
-#       redirect_to question_path(params[:question_id])
-#     elsif params.has_key?('answer_id')
-#       answer = Answer.find(params[:answer_id])
-#       answer.comments.create(content: params[:comment][:content], user_id: session[:id])
-#       redirect_to question_path(answer.question_id)
-#     end
-#   end
-
-
- # def new 
- #    if params.has_key?('question_id')
- #      @comment = Comment.new
- #      @commentable = Question.find(params[:question_id])
- #    elsif params.has_key?('answer_id')
- #      @comment = Comment.new
- #      @commentable = Answer.find(params[:answer_id])
- #    end
- #  end 
