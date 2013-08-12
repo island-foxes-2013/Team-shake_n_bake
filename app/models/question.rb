81954656720d8d2d8d24dc87cd392223216aa4df
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-08-12 10:58:50 -0700

    remove :user_id from attr_accessible to avoid security risk

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ActiveRecord::Base
   has_many :votes, as: :votetable
   has_many :comments, as: :commentable
   belongs_to :user
-  attr_accessible :title, :body, :user_id, :image
+  attr_accessible :title, :body, :image
   validates :title, :body, presence: true
 
   mount_uploader :image, Uploader
