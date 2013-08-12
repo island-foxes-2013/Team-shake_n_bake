37076d73401846f3e125317a356e15f5eff35d1a
Christine <cmarquardt86@gmail.com>
2013-08-11 22:53:19 -0700

    Fix answering questions spec, remove show page in favor of AJAX

--- spec/features/answering_question_spec.rb
+++ spec/features/answering_question_spec.rb
@@ -4,35 +4,30 @@ describe "answering a question" do
 
 	before do
 		@question = create(:question)
-		@user = create(:user)
-		login_as(@user)
 	end
 
-	context "on new answer page" do
-			it "can create a new answer to a question" do
-				visit new_question_answer_path(@question)
-
-				expect {
-					fill_in 'answer_body', with: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
-					click_button "Submit Answer!"
-					}.to change(Answer, :count).by(1)
-
-					page.should have_content("Answers to this question:")
-			end
+	context "on new answer page", js: true do
+		before do
+			@user = create(:user)
+			login_as(@user)
 		end
-
-		context "on question show page" do
-			it "can go to the answer creation form by clicking on the Answer This Question! link" do
-				visit question_path(@question)
-				click_link("Answer This Question!")
-				page.should have_content("Answer this question:")
+	
+		it "can create a new answer to a question" do
+			visit question__path(@question)
+			click_on "Be the first to answer this question!"
+			fill_in "answer_body", with: "The first answer"
+			click_on "Create Answer"
+			within(".answer_box") do
+				page.should have_content("The first answer")
 			end
-		end
-
-		context "on question show page" do
-			it "should not have the Answer This Question! link on page when not signed in" do
-				visit questions_path(@question)
-				page.should_not have_content("Answer this question!")
-			end 
 		end
 	end
+
+
+	context "on question show page" do
+		it "should not let the logged out user answer this question" do
+			visit questions_path(@question)
+			page.should_not have_content("Be the first to answer this question!")
+		end 
+	end
+end
